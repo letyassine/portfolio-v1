42b2cea0119481bd6c3ac20ec639b52cd4310b83
yassinehaimouch <haymouch53@gmail.com>
2024-03-17 22:13:36 +0000

    Add blog and other pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
-const { withContentlayer } =  require('next-contentlayer')
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true }
+const nextConfig = {};
 
-module.exports = withContentlayer(nextConfig)
+module.exports = withContentlayer(nextConfig);
