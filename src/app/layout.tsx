059a2c99f54241834673ccd85caef75dc17cced3
Yassine Haimouch <haymouch53@gmail.com>
2024-08-07 13:15:12 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,10 +36,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <body
         className={cn(
-          "antialiased bg-slate-50 dark:bg-dark pt-[60px] md:pt-[48px]",
+          "bg-slate-50 dark:bg-dark pt-[60px] md:pt-[48px]",
           dmsans.className
         )}
       >
